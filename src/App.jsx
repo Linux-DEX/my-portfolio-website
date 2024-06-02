e766c42aad23e56e8794cbe2555348977e72ec98
Linux-DEX <sarabjeet.9353@gmail.com>
2024-06-02 21:38:13 +0530

    navigation bar partial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <>
-      <h1>Portfolio website</h1>
+      <Navbar />
+      <h1>Home page body</h1>
     </>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import "./Navbar.css"
+import { LinearGradient } from 'react-text-gradients'
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <div className="left-navbar">
+            <LinearGradient
+              className='left-navbar-logo'
+              gradient={['to left', '#17acff ,#ff68f0']}
+            >
+                 
+            </LinearGradient>
+            <LinearGradient
+              className='left-navbar-text'
+              gradient={['to left', '#17acff ,#ff68f0']}
+            >
+              Linux-DEX
+            </LinearGradient>
+        </div>
+        <div className="right-navbar">
+          <ul>
+            <li>
+              <a href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                My Work
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Experience
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="space"></div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
